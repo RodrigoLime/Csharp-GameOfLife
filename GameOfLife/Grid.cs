2dee5262f8fa22636141338b251a0be1bffc88b3
RodrigoLime <rodrigo.machadoassis14@gmail.com>
2024-04-16 22:48:57 -0300

    Add project files.

---
+++ GameOfLife/Grid.cs
@@ -0,0 +1,115 @@
+﻿using System.Text;
+
+namespace GameOfLife;
+
+public class Grid
+{
+    
+    private Pixel[][] pixels { get; set; } //Grid pixels
+
+    public Grid(char[][] matrix) //Grid constructor, it receives a char matrix and transforms it into a pixel matrix
+    {
+        pixels = new Pixel[matrix.Length][];
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            pixels[i] = new Pixel[matrix[i].Length];
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                pixels[i][j] = new Pixel(matrix[i][j]);
+            }
+        }
+
+        this.pixels = pixels;
+    }
+
+    public Pixel? GetPixel(int row, int column) //Function to get a pixel in a specific location in the grid
+    {
+        if (pixels[row][column] != null)
+            return pixels[row][column];
+        else
+            return null;
+                
+    }
+
+    public void Build() // Function to attribute top, left, etc pixels to each pixel in the grid
+    {
+       for (int i = 0; i < pixels.Length; i++)
+        {
+            for(int j = 0; j < pixels[i].Length; j++)
+            {   
+                if (i > 0) //If not in top row, it has a top
+                    pixels[i][j].top = pixels[i - 1][j]; //attributes the top pixel as the top for the pixel
+                if (i > 0  && j < (pixels[i].Length - 1)) //if not in top row and not in right col, it has a topright, the rest of this function has the same logic
+                    pixels[i][j].topright = pixels[i - 1][j + 1];
+                if (j < pixels[i].Length - 1)
+                    pixels[i][j].right = pixels[i][j + 1];
+                if (i < pixels.Length - 1 && j < pixels[i].Length - 1)
+                    pixels[i][j].bottomright = pixels[i + 1][j + 1];
+                if (i < pixels.Length - 1)
+                    pixels[i][j].bottom = pixels[i + 1][j];
+                if (i < pixels.Length - 1 && j > 0)
+                    pixels[i][j].bottomleft = pixels[i + 1][j - 1];
+                if (j > 0)
+                    pixels[i][j].left = pixels[i][j - 1];
+                if (i > 0 && j > 0)
+                    pixels[i][j].topleft = pixels[i - 1][j - 1];
+            }
+        }
+    }
+
+
+    public void Refresh() //Applies the Conway's Game of Life logic here, refreshing the grid once
+    {
+        Pixel[][] aux = new Pixel[pixels.Length][];
+ 
+        
+        for (int i = 0; i < pixels.Length; i++) //This iteration copies the content of the original grid to an auxiliary one so that it doesnt mess up the logic
+        {
+            aux[i] = new Pixel[pixels[i].Length];
+            for (int j = 0; j < pixels[i].Length; j++)
+            {
+                aux[i][j] = new Pixel(pixels[i][j].content); 
+            }
+        }
+
+
+        for (var i = 0; i < pixels.Length; i++) //Logic
+        {
+            for (var j = 0; j < pixels[i].Length; j++)
+            {
+                if (pixels[i][j].content == 'O' && (pixels[i][j].Adjacents() <= 1 || pixels[i][j].Adjacents() >= 4)) //A live pixel that has <=1 or >=4 neighbors dies
+                {
+                    aux[i][j].content = ' ';
+                }
+                else if (pixels[i][j].content == ' ' && pixels[i][j].Adjacents() == 3) //An empty pixel that has exactly 3 neighbors becomes activated
+                {
+                    aux[i][j].content = 'O';
+                }
+                //Else, a live pixel that has 2 or 3 neighbors survives
+            }
+        }
+
+        for (int i = 0; i < pixels.Length; i++) //Rebuilds the pixels matrix so that it can be used in the next iteration
+        {
+            for (int j = 0; j < pixels[i].Length; j++)
+            {
+                pixels[i][j].content = aux[i][j].content;
+            }
+        }
+    }
+
+    public override string ToString() //Prints the grid
+    {
+        StringBuilder grid = new StringBuilder();
+        foreach (var row in pixels)
+        {
+            foreach (var elem in row)
+            {
+                grid.Append(elem.content);
+            }
+            grid.Append("\n");
+        }
+        return grid.ToString();
+    }
+}
---
+++ GameOfLife/Pixel.cs
@@ -0,0 +1,43 @@
+﻿namespace GameOfLife;
+
+public class Pixel
+{
+    public char content { get; set; } //Indicates if the pixel is activated or not
+
+    //Properties indicating adjacent pixels
+    public Pixel? top { get; set; }
+    public Pixel? bottom { get; set; }
+    public Pixel? left { get; set; }
+    public Pixel? right { get; set; }
+    public Pixel? topleft { get; set; }
+    public Pixel? bottomleft { get; set; }
+    public Pixel? topright { get; set; }
+    public Pixel? bottomright { get; set; }
+
+    public Pixel(char content)
+    {
+        this.content = content;
+    }
+
+    public int Adjacents() //Function that returns how many adjacent pixels are activated
+    {
+        int i = 0;
+        if (top != null && top.content == 'O') { i++; }
+        if (topright != null && topright.content == 'O') { i++; }
+        if (right != null && right.content == 'O') { i++; }
+        if (bottomright != null && bottomright.content == 'O') { i++; }
+        if (bottom != null && bottom.content == 'O') { i++; }
+        if (bottomleft != null && bottomleft.content == 'O') { i++; }
+        if (left != null && left.content == 'O') { i++; }
+        if (topleft != null && topleft.content == 'O') { i++; }
+
+        return i;
+
+    }
+
+    public char Print() //Prints a pixel
+    { 
+        return content;
+    }
+
+}
